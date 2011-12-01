0bb1cec4b783a417bc9a8b08a04d1b2d0fec5440
Armando Gonzalez <iam@armando.mx>
2011-12-01 14:38:51 -0600

    Store manduks

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Store::Application.config.secret_token = 'd69eda6d59a01dfbdcc41eee24aca47552df92d04ad35f3c6afdcd001eb33ccf0b602b99646c38b852155b7522abebf179aa1a72287e8d10d0ac249a02823303'
